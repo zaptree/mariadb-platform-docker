66627cdf1248873e80c8d1ccf7205804fd922ca7
WonkyWumpus <Chief@WonkyWumpus.com>
2019-01-15 18:31:04 -0800

    Fixing a few issues with binlog routing.

--- single-container/scripts/mariadb-server-ms-init.sh
+++ single-container/scripts/mariadb-server-ms-init.sh
@@ -5,20 +5,24 @@ sudo -u mysql2 /usr/local/mysql/scripts/mysql_install_db --datadir=/var/lib/mysq
 sudo -u mysql2 /usr/local/mysql/scripts/mysql_install_db --datadir=/var/lib/mysql/3/ --basedir=/usr/local/mysql
 
 /usr/local/mysql/bin/mysqld --defaults-file=/etc/mariadb-server-1.cnf &
-/usr/local/mysql/bin/mysqld --defaults-file=/etc/mariadb-server-2.cnf &
-/usr/local/mysql/bin/mysqld --defaults-file=/etc/mariadb-server-3.cnf &
-
-sleep 45
-
+sleep 15
+echo "Executing init 1"
 /usr/local/mysql/bin/mysql -h 127.0.0.1 -P 33061 < /home/mysql2/scripts/mariadb-server-ms-init.sql
+
+/usr/local/mysql/bin/mysqld --defaults-file=/etc/mariadb-server-2.cnf &
+sleep 15
+echo "Executing init 2"
 /usr/local/mysql/bin/mysql -h 127.0.0.1 -P 33062 < /home/mysql2/scripts/mariadb-server-ms-init.sql
+
+/usr/local/mysql/bin/mysqld --defaults-file=/etc/mariadb-server-3.cnf &
+sleep 15
+echo "Executing init 3"
 /usr/local/mysql/bin/mysql -h 127.0.0.1 -P 33063 < /home/mysql2/scripts/mariadb-server-ms-init.sql
 
-sleep 45
 
 /usr/local/mysql/bin/mysql -h 127.0.0.1 -P 33061 < /home/mysql2/scripts/mariadb-server-ms-seed.sql
 
-sleep 45
+sleep 15
 
 if [ -f /var/run/mysqld/mysqld1.pid ]; then
   kill `cat /var/run/mysqld/mysqld1.pid`
